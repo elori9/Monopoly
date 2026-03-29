fce206e543b2fc3dc9a51753864bed6b218b47be
elori9 <originaly55@gmail.com>
2026-03-29 13:30:32 +0200

    Dice class

--- app/src/main/java/game/model/Dice.kt
+++ app/src/main/java/game/model/Dice.kt
@@ -1,4 +1,23 @@
 package game.model
 
+import kotlin.random.Random
+
 class Dice {
+    // Use private set for make it readable and non-writeable
+    var numbers: Int = 0
+        private set
+
+    /**
+     * Rolls 2 dices
+     *
+     * @return The result of both combined (2-12)
+     */
+    fun roll(): Int {
+        val dice1 = Random.nextInt(1, 7)
+        val dice2 = Random.nextInt(1, 7)
+
+        numbers = dice1 + dice2
+
+        return numbers
+    }
 }
