cc591eb5d01672117670bc3763bd4c99ca1329f2
elori9 <originaly55@gmail.com>
2026-04-02 16:31:26 +0200

    Improved roll animation, now there is a roll of 360 degrees in a time of 250ms with a linear velocity, doc: https://developer.android.com/develop/ui/compose/animation/value-based?hl=es-419 AND https://developer.android.com/develop/ui/compose/animation/customize?hl=es-419

--- app/src/main/java/com/example/monopoly/ui/components/animations/AnimatedDice.kt
+++ app/src/main/java/com/example/monopoly/ui/components/animations/AnimatedDice.kt
@@ -1,16 +1,22 @@
 package com.example.monopoly.ui.components.animations
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,9 +24,29 @@ import androidx.compose.ui.unit.sp
 import com.example.monopoly.R
 import game.model.Dice
 
-
+/**
+ * Roll dice animation
+ */
 @Composable
 fun RollDice(modifier: Modifier = Modifier, result: Int, onRollClick: () -> Unit) {
+    // Rotation state
+    var rotation by remember { mutableFloatStateOf(0f) }
+
+    // Animate
+    val rotationDice by animateFloatAsState(
+        targetValue = rotation,
+        animationSpec = tween (durationMillis = 250, easing = LinearEasing),
+        label = "rotation"
+    )
+
+    // Recreate when rotation change state
+    LaunchedEffect (result) {
+        if (result != 1 || rotation != 0f) {
+            // For just rolling on click
+            rotation += 360f
+        }
+    }
+
     val imageResource = when (result) {
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
@@ -32,9 +58,12 @@ fun RollDice(modifier: Modifier = Modifier, result: Int, onRollClick: () -> Unit
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
         Image(
             painter = painterResource(imageResource),
-            contentDescription = result.toString()
+            contentDescription = result.toString(),
+            // Make rotation
+            modifier = Modifier.graphicsLayer{
+                rotationZ = rotationDice
+            }
         )
-
         Button(
             onClick = onRollClick,
         ) {
