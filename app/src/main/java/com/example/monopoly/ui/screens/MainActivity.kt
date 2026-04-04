560d842b112306b38ac90cc19db865fe3f631953
elori9 <originaly55@gmail.com>
2026-04-04 18:44:02 +0200

    Fixed multiple orientations on main screen and added background img for both

--- app/src/main/java/com/example/monopoly/ui/screens/MainActivity.kt
+++ app/src/main/java/com/example/monopoly/ui/screens/MainActivity.kt
@@ -1,19 +1,27 @@
 package com.example.monopoly.ui.screens
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,83 +37,202 @@ class MainActivity : ComponentActivity() {
         setContent {
             MonopolyTheme {
                 val context = LocalContext.current
+                val isPortrait =
+                    LocalConfiguration.current.orientation == Configuration.ORIENTATION_PORTRAIT
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MainMenuScreen(
-                        modifier = Modifier.padding(innerPadding),
-                        onNewGameClick = {
-                            startActivity(Intent(context, ConfigActivity::class.java))
-                        },
-                        onHelpClick = {
-                            startActivity(Intent(context, HelpActivity::class.java))
-                        },
-                        onExitClick = {
-                            exitProcess(0)
-                        })
+                    if (isPortrait) {
+                        MainMenuScreenPortrait(
+                            modifier = Modifier.padding(innerPadding),
+                            onNewGameClick = {
+                                startActivity(Intent(context, ConfigActivity::class.java))
+                            },
+                            onHelpClick = {
+                                startActivity(Intent(context, HelpActivity::class.java))
+                            },
+                            onExitClick = {
+                                exitProcess(0)
+                            })
+                    } else {
+                        MainMenuScreenLandscape(
+                            modifier = Modifier.padding(innerPadding),
+                            onNewGameClick = {
+                                startActivity(Intent(context, ConfigActivity::class.java))
+                            },
+                            onHelpClick = {
+                                startActivity(Intent(context, HelpActivity::class.java))
+                            },
+                            onExitClick = {
+                                exitProcess(0)
+                            })
+                    }
                 }
             }
         }
     }
 }
 
+/**
+ * Portrait menu stateless
+ */
 @Composable
-fun MainMenuScreen(
+fun MainMenuScreenPortrait(
     modifier: Modifier = Modifier,
     onNewGameClick: () -> Unit,
     onHelpClick: () -> Unit,
     onExitClick: () -> Unit
 ) {
-
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = stringResource(R.string.app_name),
-            fontSize = 64.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 64.dp)
+    Box(modifier = modifier.fillMaxSize()) {
+        // Background
+        Image(
+            painter = painterResource(id = R.drawable.verticalbackground),
+            contentDescription = "Background",
+            contentScale = ContentScale.Crop, // Fill on the screen
+            modifier = Modifier.fillMaxSize()
         )
 
-        val buttonModifier = Modifier
-            .width(250.dp)
-            .height(80.dp)
-            .padding(vertical = 8.dp)
-
-        Button(
-            onClick = onNewGameClick,
-            modifier = buttonModifier,
-            shape = RoundedCornerShape(16.dp),
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF03A9F4))
+        Column(
+            modifier = modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = stringResource(R.string.NewGame), fontSize = 24.sp, fontWeight = FontWeight.Bold)
-        }
-
-        Button(
-            onClick = onHelpClick,
-            modifier = buttonModifier,
-            shape = RoundedCornerShape(16.dp),
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF03A9F4))
-        ) {
-            Text(text = stringResource(R.string.Help), fontSize = 24.sp, fontWeight = FontWeight.Bold)
-        }
-
-        Button(
-            onClick = onExitClick,
-            modifier = buttonModifier,
-            shape = RoundedCornerShape(16.dp),
-            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF03A9F4))
-        ) {
-            Text(text = stringResource(R.string.Exit), fontSize = 24.sp, fontWeight = FontWeight.Bold)
+            Text(
+                text = stringResource(R.string.app_name),
+                fontSize = 64.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 30.dp),
+                style = TextStyle( // Put color white with black border
+                    color = Color.White,
+                    shadow = Shadow(
+                        color = Color.Black,
+                        offset = Offset(4f, 4f),
+                        blurRadius = 8f
+                    )
+                ),
+            )
+            val buttonModifier = Modifier
+                .width(250.dp)
+                .height(80.dp)
+                .padding(vertical = 8.dp)
+            Buttons(
+                modifier = buttonModifier,
+                onNewGameClick = onNewGameClick,
+                onHelpClick = onHelpClick,
+                onExitClick = onExitClick
+            )
         }
     }
 }
 
-@Preview(showBackground = true)
+/**
+ * Landscape main menu stateless
+ */
+@Composable
+fun MainMenuScreenLandscape(
+    modifier: Modifier = Modifier,
+    onNewGameClick: () -> Unit,
+    onHelpClick: () -> Unit,
+    onExitClick: () -> Unit
+) {
+    Box(modifier = modifier.fillMaxSize()) {
+        // Background
+        Image(
+            painter = painterResource(id = R.drawable.horizaontalbackground),
+            contentDescription = "Background",
+            contentScale = ContentScale.Crop, // Fill on the screen
+            modifier = Modifier.fillMaxSize()
+        )
+
+        // Top
+        Column(
+            modifier = modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(R.string.app_name),
+                fontSize = 64.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 30.dp),
+                style = TextStyle( // Put color white with black border
+                    color = Color.White,
+                    shadow = Shadow(
+                        color = Color.Black,
+                        offset = Offset(4f, 4f),
+                        blurRadius = 8f
+                    )
+                ),
+            )
+            val buttonModifier = Modifier
+                .width(300.dp)
+                .height(60.dp)
+                .padding(vertical = 8.dp)
+            Buttons(
+                modifier = buttonModifier,
+                onNewGameClick = onNewGameClick,
+                onHelpClick = onHelpClick,
+                onExitClick = onExitClick
+            )
+        }
+    }
+}
+
+/**
+ * Buttons
+ */
+@Composable
+fun Buttons(
+    modifier: Modifier,
+    onNewGameClick: () -> Unit,
+    onHelpClick: () -> Unit,
+    onExitClick: () -> Unit
+) {
+
+    Button(
+        onClick = onNewGameClick,
+        modifier = modifier,
+        shape = RoundedCornerShape(16.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF03A9F4))
+    ) {
+        Text(
+            text = stringResource(R.string.NewGame),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+
+    Button(
+        onClick = onHelpClick,
+        modifier = modifier,
+        shape = RoundedCornerShape(16.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF03A9F4))
+    ) {
+        Text(
+            text = stringResource(R.string.Help),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+
+    Button(
+        onClick = onExitClick,
+        modifier = modifier,
+        shape = RoundedCornerShape(16.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF03A9F4))
+    ) {
+        Text(
+            text = stringResource(R.string.Exit),
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Preview(showBackground = true, widthDp = 300, heightDp = 600)
 @Composable
 fun MainMenuPreview() {
     MonopolyTheme {
-        MainMenuScreen(
+        MainMenuScreenPortrait(
             onNewGameClick = {},
             onHelpClick = {},
             onExitClick = {}
